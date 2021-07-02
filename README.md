359a885f1812f633ec547e335e035e9572c3c28e
An0nySec <anonysec@vip.qq.com>
2021-07-02 14:18:50 +0800

    UserAdd

---
+++ README.md
@@ -0,0 +1,18 @@
+### Introduce
+
+**UserAdd.exe   (.NET Framework 2.0)**
+
+```tex
+Usage: UserAdd.exe <username> <password>
+```
+
+<img src=".\imgs\image-20210702141149756.png" alt="image-20210702141149756" style="zoom:50%;" />
+
+1. 利用`DirectoryService` 命名空间添加用户
+2. 将用户添加到管理员与远程桌面组
+
+------
+
+### Reference
+
+- [使用目录服务和 Visual C 升在本地系统中添加用户](https://docs.microsoft.com/zh-cn/troubleshoot/dotnet/csharp/add-user-local-system)
---
+++ UserAdd/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.DirectoryServices;
+
+
+// Reference:https://docs.microsoft.com/zh-cn/troubleshoot/dotnet/csharp/add-user-local-system
+
+namespace UserAdd
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: UserAdd.exe <username> <password>");
+                //Console.WriteLine("       UserAdd.exe guest password (Guest enabled)");
+            }
+            else
+            {
+                string username = args[0];
+                string password = args[1];
+                try
+                {
+                    DirectoryEntry AD = new DirectoryEntry("WinNT://" + Environment.MachineName + ",computer");
+                    DirectoryEntry NewUser = AD.Children.Add(username, "user");
+                    NewUser.Invoke("SetPassword", new object[] { password });
+                    //NewUser.Invoke("Put", new object[] { "Description", "Test User from .NET" });
+                    NewUser.CommitChanges();
+                    DirectoryEntry grp;
+
+                    grp = AD.Children.Find("Administrators", "group");
+                    if (grp != null) { grp.Invoke("Add", new object[] { NewUser.Path.ToString() }); }
+                    grp = AD.Children.Find("Remote Desktop Users", "group");
+                    if (grp != null) { grp.Invoke("Add", new object[] { NewUser.Path.ToString() }); }
+                    Console.WriteLine("Account Created Successfully");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+        }
+    }
+}
